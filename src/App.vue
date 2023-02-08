235960439ec52d9872cd455cf6d6a926c7e06749
Amudipe Emmanuel <abiemma84@gmail.com>
2023-02-08 12:45:45 +0000

    Worked on the color picker game

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script>
 import ColorPickerGame from "./components/ColorPickerGame.vue";
 import Recap_01_02_2023 from "./components/Recap_01_02_2023.vue";
-
 export default {
   components: { ColorPickerGame, Recap_01_02_2023 },
   data() {
